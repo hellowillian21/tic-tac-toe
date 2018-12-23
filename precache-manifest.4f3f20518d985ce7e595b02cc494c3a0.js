569d5a0c0d7e545482503b9e9d27750cbea4ac47
willian <hellowillian21@gmail.com>
2018-12-23 21:23:01 +0800

    Updates

---
+++ precache-manifest.4f3f20518d985ce7e595b02cc494c3a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "bf52fe0b1d57e204336d",
+    "url": "/tic-tac-toe/static/js/main.bf52fe0b.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/tic-tac-toe/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "bf52fe0b1d57e204336d",
+    "url": "/tic-tac-toe/static/css/main.5286fd27.chunk.css"
+  },
+  {
+    "revision": "ecebc94d7c399d894f8c514aa7c71daf",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
